65c2db17469f880d88bc2af270c5e08803e5cd95
MarioDevelop3r <pcservicecurico@gmail.com>
2024-09-17 20:50:47 -0300

    Refactorizar estilos y agregar funcionalidad de limpiar campo de contraseña
    Se refactorizaron los estilos del archivo index.html y se agregó la funcionalidad de limpiar el campo de contraseña en el archivo main.js. Además, se implementó una validación de seguridad de la contraseña generada, mostrando un mensaje de alerta si la contraseña no cumple con los requisitos mínimos de seguridad.
    
    Desafíos abordados:
    - Desafío 1: Se agregaron caracteres especiales !@#$%^&*() a la lista de caracteres permitidos para generar contraseñas.
    - Desafío 2: Se agregó un botón para limpiar el campo de contraseña.
    - Desafío 3: Se implementó una validación de seguridad de la contraseña generada, mostrando un mensaje de alerta si la contraseña no cumple con los requisitos mínimos de seguridad.

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 let cantidad = document.getElementById("cantidad"); // Campo de entrada donde el usuario ingresa la cantidad de caracteres
 let boton = document.getElementById("generar"); // Botón que el usuario presiona para generar la contraseña
 let contrasena = document.getElementById("contrasena"); // Campo de texto donde se mostrará la contraseña generada
+let botonLimpiar = document.getElementById("limpiar"); // Botón que el usuario presiona para limpiar el campo de contraseña
 
 // Definimos los caracteres permitidos en la contraseña
-const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"; // Se agregan caracteres especiales !@#$%^&*()  desafio 1 Christian Velasco
+const caracteres =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"; // Se agregan caracteres especiales !@#$%^&*()  desafio 1 Christian Velasco
 
 // Función que se ejecuta al hacer clic en el botón "Generar"
 function generar() {
@@ -23,21 +25,46 @@ function generar() {
   // Bucle para generar cada carácter de la contraseña
   for (let i = 0; i < numeroDigitado; i++) {
     // Generar un carácter aleatorio de la lista de caracteres permitidos
-    let caracterAleatorio = caracteres[Math.floor(Math.random() * caracteres.length)];
+    let caracterAleatorio =
+      caracteres[Math.floor(Math.random() * caracteres.length)];
     // Añadir el carácter aleatorio a la contraseña
     password += caracterAleatorio;
+  }
+
+  // validacion de la seguridad de la contraseña
+  let tieneMayusculas = /[A-Z]/.test(password);
+  let tieneMinusculas = /[a-z]/.test(password);
+  let tieneNumeros = /[0-9]/.test(password);
+  let tieneCaracteresEspeciales = /[!@#$%^&*()]/.test(password);
+
+  if (
+    tieneMayusculas &&
+    tieneMinusculas &&
+    tieneNumeros &&
+    tieneCaracteresEspeciales
+  ) {
+    alert("Contraseña segura");
+  } else {
+    alert(
+      "Contaseña no segura dece debe incluir al memos una mayuscula, una minuscula, un numero y un caracter especial"
+    );
   }
 
   // Asignar la contraseña generada al campo de texto para mostrarla al usuario
   contrasena.value = password;
 }
 
+// Funcion para limpiar el campo de contraseña
+function limpiarCampo() {
+  contrasena.value = "";
+}
+
 // Asignar la función generar al evento 'click' del botón
 boton.addEventListener("click", generar);
+botonLimpiar.addEventListener("click", limpiarCampo); // Desafio 2 Leo Castillo . agregar un boton limpiar campo contraseña
 
-  // Pendientes de mejora :  
-  
+// Pendientes de mejora :
+
 // Desafio 1 Christian Velasco agregar caracteres especiales !@#$%^&*() - ok
-// Desafio 2 Leo Castillo . agregar un boton limpiar campo contraseña
-// Desafio 3 Jeanmarie Quijada  agregar alert con mensajes de validacion del tipo de contraseña minimo mayusculas, minusculas, numeros y caracteres especiales
-
+// Desafio 2 Leo Castillo . agregar un boton limpiar campo contraseña - ok
+// Desafio 3 Jeanmarie Quijada  agregar alert con mensajes de validacion del tipo de contraseña minimo mayusculas, minusculas, numeros y caracteres especiales y debe mostrar mensaje de contraseña segura o no segura -ok
